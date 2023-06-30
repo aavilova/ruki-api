42d8280db42e93a9d451d48e9d47e2ecae189bb4
Alisa Avilova <aalisa@MacBook-Pro-Alisa.local>
2023-06-30 09:22:01 +0300

    all

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,3 +1,23 @@
 class Api::V1::Users::SessionsController < Devise::SessionsController
   respond_to :json
+  # skip_before_action :authenticate_request, only: [:create]
+  # # def create
+  # #   user = User.find_by(email: params[:email])
+
+  # # #   # if user&.authenticate(params[:password])
+  # # #   #   token = Token.encode({ user_id: user.id })
+  # # #   #   render json: { token: token, status: 'Logged in successfully' }, status: :ok
+  # # #   # else
+  # # #   #   render json: { error: 'Invalid email or password' }, status: :unauthorized
+  # # #   # end
+  # # end
+
+  # # # def destroy
+  # # #   # Implement your logout logic here
+  # # #   render json: { status: 'Logged out successfully' }, status: :ok
+  # # # end
+
+  # # def log_in_params
+  # #   params.require(:user).permit(:email, :password)
+  # # end
 end
--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -6,15 +6,15 @@ class SubscribersController < ApplicationController
     def create
       @subscriber = Subscriber.new(subscriber_params)
       if @subscriber.save
-        redirect_to about_path, notice: "Ура!"
+       
       else
-        redirect_to about_path, notice: "Не ура("
+
       end
     end
   
     private
   
       def subscriber_params
-        params.require(:subscriber).permit(:email)
+        params.require(:subscriber).permit(:name, :email)
       end
   end
